5a4713ce543cd671057220323f37fdbc72edc647
Paul Sticea <paul.sticea@xivic.com>
2013-03-01 12:42:34 +0200

    update bootstrap style

---
+++ app/scripts/filters/pagination.js
@@ -0,0 +1,16 @@
+"use strict";
+
+movieMakerApp.filter('pagination', function() {
+	return function(input, currentPage, itemPerPage, pagedItems) {
+		itemPerPage = itemPerPage;
+		var start = currentPage * itemPerPage,
+			end = start + itemPerPage;
+		for (var i=start; i < end; i++)
+			if(pagedItems[i]){
+				input.push(pagedItems[i]);
+			}else{
+				return input;
+			}
+		return input;
+	};
+});
